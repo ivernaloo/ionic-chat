e543c2f45632d307d7c0057225e8b5c9d869628f
aloo <aloocs@gmail.com>
2016-07-14 18:43:00 +0800

    同步了基本的代码

---
+++ ionic-chat-app/www/js/app.services.js
@@ -0,0 +1,38 @@
+angular.module('ionic-chat-app-services', [])
+  .service('ChatService', function ChatService($rootScope) {
+
+    // Init the Websocket connection
+    var socket = io.connect('http://localhost:8080');
+
+    // Bridge events from the Websocket connection to the rootScope
+    socket.on('UserConnectedEvent', function(user){
+      $rootScope.emit('UserConnectedEvent', user); // publish the global event
+    });
+    socket.on('MessageReceivedEvent', function(message){
+      $rootScope.emit('MessageReceivedEvent', message); // publish global event
+    });
+
+    /*
+    * Send a message to the server
+    * @param message
+    * */
+    this.emit = function (message) {
+      socket.emit('MessageSentEvent', message);  // send the message to the server
+    };
+    this.on = {
+      // user connect
+      userConnected: function (callback) {
+        $rootScope.$on('UserConnectedEvent',
+          function (event, user) {
+            callback(user);
+          })
+      },
+      // mesage received
+      messageReceived: function (callback) {
+        $rootScope.$on('MessageReceivedEvent',
+          function (event, message) {
+            callback(message);
+          })
+      }
+    }
+  });
